570e1096318e9fffe00a2d2c24088806c031269e
Diana Padilla <tu-correo@ejemplo.com>
2026-03-12 16:21:44 -0500

    fix base path github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/catastro-eps-moyobamba/',
   plugins: [react()],
-  base: '/catastro-eps-moyobamba/', 
 })
