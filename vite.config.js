c53b5f588632d2ba0fc428e4528e99e556a4f1e6
lady12345678 <58115888+lady12345678@users.noreply.github.com>
2026-03-13 02:36:40 -0500

    Fix base path comment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-  base: "/catastro-eps-moyobamba/",
   plugins: [react()],
-});
+  base: '/catastro-eps-moyobamba/', // ESTO ES VITAL
+})
