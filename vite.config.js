99078c0f632d36bc6bcc628e3763c8ee83f18e00
lady12345678 <58115888+lady12345678@users.noreply.github.com>
2026-03-13 02:37:29 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/catastro-eps-moyobamba/', // ESTO ES VITAL
+  // El "base" debe ser exactamente el nombre de tu repositorio en GitHub
+  // encerrado entre barras diagonales /
+  base: '/catastro-eps-moyobamba/', 
 })
